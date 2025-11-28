e392f057d09aa3c012330ff03f17532c69b2a349
Emilia Rees-Heseltine <E.Rees-Heseltine@albynschool.org>
2025-11-28 15:35:03 +0000

    fgsvhythrurtjhegftghyjgtfr

--- SDD/#Temp tracker - Task 2.py
+++ SDD/#Temp tracker - Task 2.py
@@ -1,11 +1,12 @@
-#Temp tracker - Task 1
+#Temp tracker - Task 2
 
 #set things
 totalTemps = 0
 temps = []
+TEMP = 0
 
 #loooooooop
-for index in range(5):
+for index in range(14):
     tempIn = float(input("Input a temperature between -20 and 50 please."))
     while tempIn < -20 or tempIn > 50:
         print("Error: Temperature was not between -20 and 50. D:")
